da2349e6e1c52705a9b199e0745b0259dfbcf5ff
Balaji <balaji.hariharan.work@gmail.com>
2016-11-28 23:25:01 -0500

    added testcase for wrapper

--- src/test/java/com/sample/pricing/service/PricingServiceWrapperTest.java
+++ src/test/java/com/sample/pricing/service/PricingServiceWrapperTest.java
@@ -37,5 +37,26 @@ public class PricingServiceWrapperTest {
 		assertThat(recommendedPriceList.get(0).getRecommendedPrice(),equalTo(0.9));
 		assertThat(recommendedPriceList.get(1).getRecommendedPrice(),equalTo(10.5));
 	}
-
+	@Test
+	public void testInput2() {
+		// These details could come from making a Rest call/DB or as .CSV file
+		List<String> productTypes = new ArrayList<String>();
+		productTypes.add("mp3player H H");
+		productTypes.add("ssd L L");
+		List<String>productList = new ArrayList<String>();
+ 
+		productList.add("ssd W 11.0");
+		productList.add("ssd X 12.0");
+		productList.add("mp3player X 60.0");
+		productList.add("mp3player Y 20.0");
+		productList.add("mp3player Z 50.0");
+		productList.add("ssd V 10.0");
+		productList.add("ssd Y 11.0");
+		productList.add("ssd Z 12.0");
+		List<RecommendedProduct> recommendedPriceList = pricingServiceWrapper.findRecommendedPriceForGroup(productTypes, productList);
+		assertThat(recommendedPriceList.size(), equalTo(productTypes.size()));
+		assertNotNull("RecommendedPrice List is null ",recommendedPriceList.get(0));
+		assertThat(recommendedPriceList.get(0).getRecommendedPrice(),equalTo(50.0));
+		assertThat(recommendedPriceList.get(1).getRecommendedPrice(),equalTo(12.1));
+	}
 }
